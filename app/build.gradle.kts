794de1541c4e35b0d27388c50213f23c0f724b2c
Sang <sangtovanzzz02@gmail.com>
2024-07-29 17:06:08 +0700

    add keystore, lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,26 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("kotlin-kapt")
+    id("kotlin-android")
+
+    id("androidx.navigation.safeargs")
+
+    id("com.google.gms.google-services")
+    id ("kotlin-parcelize")
 }
 
 android {
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("E:\\Workspace\\android\\N8_shopsmart\\keystore\\shopsmart.jks")
+            storePassword = "123456"
+            keyAlias = "shopsmart"
+            keyPassword = "123456"
+        }
+    }
+
     namespace = "com.example.n8_shopsmart"
     compileSdk = 34
 
@@ -33,6 +50,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +67,46 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val nav_version = "2.5.3"
+    // navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // intuit
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
+    // retrofit call api
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+
+    // gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // circle imageview
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    // slide
+    implementation("com.github.smarteist:autoimageslider:1.4.0")
+
+    // firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    // realtime
+    implementation("com.google.firebase:firebase-database")
+
+    // authentication
+    implementation("com.google.firebase:firebase-auth")
+
+    // image picker
+    implementation ("com.github.dhaval2404:imagepicker:2.1")
+
+    // storage
+    implementation("com.google.firebase:firebase-storage")
 }
